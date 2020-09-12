3fef1900c8038fa842a363ad4aaf92e993408e9b
areebirshad <areebirshad.12@gmail.com>
2020-09-12 21:58:10 +0500

    first task with getting all data

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TextInput from './component/TextInput';
-import { addTodo, deleteTodo, getAllTodos } from './services/services';
+import { addTodo, deleteTodo, getAllTodos , onEditTodo } from './services/services';
 import Spinner from './component/Spinner';
 
 function App() {
-  const [allData,setAllData] = useState([]);
+  const [allData,setAllData] = useState([]) ;
   const [isError,setError]=useState(false);
-  const [isLoading,setIsLoading]=useState(true);
+  const [isLoading,setIsLoading]  =  useState(true);
   const [todo,setTodo]=useState('');
   const [order,setOrder]=useState('');
   useEffect(()=>{
     getAllTodos()
     .then((success)=>{
-      setAllData(success);
+      let getSortedData = success.sort(function(a, b){return a.order-b.order});
+      debugger;
+      setAllData(getSortedData);
       setIsLoading(false)
     })
   },[])
@@ -32,7 +34,8 @@ function App() {
    if(todo !== '' && order !== ''){
      addTodo({title:todo,order})
      .then((success)=>{
-       setAllData((prev)=>[...prev,{title:todo,order,completed:false}])
+       setAllData((prev)=>[...prev,{title:todo,order,completed:false}].sort(function(a, b){return a.order-b.order}))
+
        setError(false)
        setTodo('');
        setOrder('')
@@ -42,6 +45,18 @@ function App() {
     }else{
       setError(true)
     }
+  }
+  const onEditClick=(id,completed)=>{
+    onEditTodo({id,completed})
+    .then((success)=>{
+      let getCloneData = [...allData];
+      let getTodoIndex = getCloneData.findIndex((item)=> item.id == id);
+      getCloneData[getTodoIndex]['completed']=true;
+      setAllData(getCloneData)
+      debugger
+    }).catch((error)=>{
+      debugger;
+    })
   }
   return (
     <div>
@@ -84,7 +99,8 @@ function App() {
     <tbody>
       
       {
-        allData && allData.map((item,index)=>(
+        allData && allData
+        .map((item,index)=>(
           <tr>
             <td>
   {index+1}
@@ -102,18 +118,16 @@ function App() {
           <i style={{marginRight:9}} onClick={()=>onDeleteTodo(item.id)} className='fas fa-trash' ></i>
 
           
-          <i onClick={()=>onDeleteTodo(item.id)} className='fas fa-pen' ></i> 
+          <i onClick={()=>onEditClick(item.id,true)} className='fas fa-pen' ></i> 
             </td>
           </tr>
         ))
       }
       <tr>
-  
       </tr>
     </tbody>
   </table> 
   }
-  
   </div>
     </div>
     </div>
